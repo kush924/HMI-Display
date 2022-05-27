743069b9b4777929fd6ae6f2ac270c2e9b0f1628
kush924 <kush924@gmail.com>
2022-05-28 03:10:44 +0530

    changes

--- trial/trial/trial.ino
+++ trial/trial/trial.ino
@@ -1,30 +1,49 @@
-String value;
-long int t;
+
+long int b;
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(115200);
+  delay(1000);
+  hmi_page("1");
+  delay(50);
+  for (int i=0; i<=120; i++){
+    hmi_text("1","1",i);
+    delay(8);
+  }
+    for (int i=120; i>=0; i--){
+    hmi_text("1","1",i);
+    delay(8);
+  }
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  t = millis();
-  if (t%10000==0)
-  {
-    hmi_page(value);
     
-if (value=="0")
-value="1";
-else
-value="0";
-  }
+    hmi_text("1","1",0);
+    
+delay(50);
+  
 }
-void hmi_page(String a){
+void hmi_page(String page_num){
   Serial.print("page page");
-    //Serial.print("\"");
-    Serial.print(value);
-    //Serial.print("\"");
+    Serial.print(page_num);
     Serial.write(0xff);
     Serial.write(0xff);
     Serial.write(0xff);
-    delay(50);
+}
+void hmi_text(String page_num,String txt_num,int txt ){
+    Serial.print("page");
+    Serial.print(page_num);
+    Serial.print(".");
+    Serial.print("t");
+    Serial.print(txt_num);
+    Serial.print(".");
+    Serial.print("txt=");
+    Serial.print("\"");
+    Serial.print(txt);
+    Serial.print("\"");
+    Serial.write(0xff);
+    Serial.write(0xff);
+    Serial.write(0xff);
 }
